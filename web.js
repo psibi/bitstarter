31ec23fb1c334bd4feec7e9b8dbf9280a470238b
Sibi <sibi@psibi.in>
2013-07-07 18:03:31 +0530

    Reads and prints index.html file now.

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+    var index_file_data = fs.readFileSync('index.html');
+    response.send(index_file_data.toString());
 });
 
 var port = process.env.PORT || 5000;
